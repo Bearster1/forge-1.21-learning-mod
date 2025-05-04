a9d62825b93274cff0edd81b9ae77464dd9ead8b
Bearster <boorman.charlie@gmail.com>
2025-05-04 19:44:02 +0100

    Added tooltips for some items

--- src/main/java/net/bearster/learningmod/item/ModItems.java
+++ src/main/java/net/bearster/learningmod/item/ModItems.java
@@ -3,14 +3,14 @@ package net.bearster.learningmod.item;
 import net.bearster.learningmod.LearningMod;
 import net.bearster.learningmod.item.custom.ChainsawItem;
 import net.bearster.learningmod.item.custom.FuelItem;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.SwordItem;
-import net.minecraft.world.item.Tier;
-import net.minecraft.world.item.Tiers;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.*;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+
+import java.util.List;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
@@ -30,10 +30,18 @@ public class ModItems {
                     .attributes(SwordItem.createAttributes(Tiers.NETHERITE, 10, -0.3f))));
 
     public static final RegistryObject<Item> ONION = ITEMS.register("onion",
-            () -> new Item(new Item.Properties().food(ModFoodProperties.ONION)));
+            () -> new Item(new Item.Properties().food(ModFoodProperties.ONION)) {
+                @Override
+                public void appendHoverText(ItemStack pStack, TooltipContext pContext, List<Component> pTooltipComponents, TooltipFlag pTooltipFlag) {
+                    pTooltipComponents.add(Component.translatable("tooltip.learningmod.onion.tooltip.1"));
+                    super.appendHoverText(pStack, pContext, pTooltipComponents, pTooltipFlag);
+                }
+            });
 
     public static final RegistryObject<Item> AURORA_ASHES = ITEMS.register("aurora_ashes",
             () -> new FuelItem(new Item.Properties(), 800));
+
+
 
 
 
--- src/main/java/net/bearster/learningmod/item/custom/ChainsawItem.java
+++ src/main/java/net/bearster/learningmod/item/custom/ChainsawItem.java
@@ -1,13 +1,19 @@
 package net.bearster.learningmod.item.custom;
 
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
+
+import java.util.List;
 
 public class ChainsawItem extends Item {
     public ChainsawItem(Properties pProperties) {
@@ -29,5 +35,17 @@ public class ChainsawItem extends Item {
         }
 
         return InteractionResult.CONSUME;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, TooltipContext pContext, List<Component> pTooltipComponents, TooltipFlag pTooltipFlag) {
+        if (!Screen.hasShiftDown()) {
+            pTooltipComponents.add(Component.translatable("tooltip.learningmod.chainsaw.tooltip.shift"));
+        } else {
+            pTooltipComponents.add(Component.translatable("tooltip.learningmod.chainsaw.tooltip.1"));
+            pTooltipComponents.add(Component.translatable("tooltip.learningmod.chainsaw.tooltip.2"));
+        }
+
+        super.appendHoverText(pStack, pContext, pTooltipComponents, pTooltipFlag);
     }
 }
