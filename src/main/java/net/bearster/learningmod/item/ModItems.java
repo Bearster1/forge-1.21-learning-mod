b4cd2520b844687e2e842563eeb41baeadeb1c2e
Bearster <boorman.charlie@gmail.com>
2025-05-04 16:57:39 +0100

    Added a chainsaw

--- src/main/java/net/bearster/learningmod/item/ModItems.java
+++ src/main/java/net/bearster/learningmod/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.bearster.learningmod.item;
 
 import net.bearster.learningmod.LearningMod;
+import net.bearster.learningmod.item.custom.ChainsawItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -17,6 +18,8 @@ public class ModItems {
     public static final RegistryObject<Item> RAW_AZURITE = ITEMS.register("raw_azurite",
             () -> new Item(new Item.Properties()));
 
+    public static final RegistryObject<Item> CHAINSAW = ITEMS.register("chainsaw",
+            () -> new ChainsawItem(new Item.Properties().durability(32)));
 
 
     public static void register(IEventBus eventBus) {
