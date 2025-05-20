cfda0240e154fa89ff2fd4d11f05fd4d1b33728e
Bearster <boorman.charlie@gmail.com>
2025-05-20 17:36:59 +0100

    Testing

--- src/main/java/net/bearster/learningmod/datagen/ModItemTagGenerator.java
+++ src/main/java/net/bearster/learningmod/datagen/ModItemTagGenerator.java
@@ -7,11 +7,9 @@ import net.bearster.learningmod.util.ModTags;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.ItemTagsProvider;
-import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Block;
-import net.minecraftforge.common.Tags;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.Nullable;
 
