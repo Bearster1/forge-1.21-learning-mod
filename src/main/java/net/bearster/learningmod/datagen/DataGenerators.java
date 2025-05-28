f3b752ee2ce900248fc76c3121a15e8605c2a40f
Bearster <boorman.charlie@gmail.com>
2025-05-28 12:03:57 +0100

    Re added painting datagen

--- src/main/java/net/bearster/learningmod/datagen/DataGenerators.java
+++ src/main/java/net/bearster/learningmod/datagen/DataGenerators.java
@@ -5,14 +5,12 @@ import net.bearster.learningmod.LearningMod;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
-import net.minecraft.data.tags.ItemTagsProvider;
 import net.minecraftforge.common.data.BlockTagsProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import org.slf4j.Logger;
-import net.bearster.learningmod.datagen.ModItemModelProvider;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -44,5 +42,9 @@ public class DataGenerators {
         generator.addProvider(event.includeServer(), new ModDataRegistryProvider(packOutput, lookupProvider));
 
         generator.addProvider(event.includeServer(), new ModWorldGenProvider(packOutput, lookupProvider));
+
+        ModPaintings modPaintings = new ModPaintings();
+        modPaintings.registerPaintings();
+
     }
 }
